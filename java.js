5dbc1ca2258037232407c21aec3a65c475f43766
SUMAN-PATI <xx_suman@yahoo.co.in>
2015-03-14 16:12:56 +0530

    uploadone

---
+++ java.js
@@ -0,0 +1,11 @@
+window.onload=function () {
+	$('#scanner').html5_qrcode(function(data){
+	 		$('#read').html(data);
+	 	},
+	 	function(error){
+			$('#read_error').html(error);
+		}, function(videoError){
+			$('#vid_error').html(videoError);
+		}
+	);
+}
